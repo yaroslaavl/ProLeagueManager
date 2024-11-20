ca5b4d4a24b964b47078f8296daf4bc7cd6f5591
yaroslaavl <ylopatkin@gmail.com>
2024-11-20 22:57:55 +0100

    20.11.2024 - run classes of microservices

---
+++ backend/admin-dashboard-service/src/main/java/org/league/app/AdminDashboardServiceApplication.java
@@ -0,0 +1,11 @@
+package org.league.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminDashboardServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminDashboardServiceApplication.class, args);
+    }
+}
---
+++ backend/calendar-event-service/src/main/java/org/league/app/CalendarEventServiceApplication.java
@@ -0,0 +1,11 @@
+package org.league.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarEventServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarEventServiceApplication.class, args);
+    }
+}
