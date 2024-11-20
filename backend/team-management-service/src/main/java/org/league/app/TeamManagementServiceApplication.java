d678718be20b0e8f4d6baef007d25066d62435d3
yaroslaavl <ylopatkin@gmail.com>
2024-11-20 22:59:31 +0100

    20.11.2024 - run classes of microservices

---
+++ backend/team-management-service/src/main/java/org/league/app/TeamManagementServiceApplication.java
@@ -0,0 +1,11 @@
+package org.league.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamManagementServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeamManagementServiceApplication.class, args);
+    }
+}
