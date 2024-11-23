c70976929a55d5bbf3153511eaf52a560452cd27
yaroslaavl <ylopatkin@gmail.com>
2024-11-23 17:49:36 +0100

    23.11.2024 - Deleted import java.util.regex.Matcher;

--- backend/user-service/src/main/java/org/league/app/validation/EmailCustomValidator.java
+++ backend/user-service/src/main/java/org/league/app/validation/EmailCustomValidator.java
@@ -3,7 +3,6 @@ package org.league.app.validation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailCustomValidator implements ConstraintValidator<EmailСustom, String> {
