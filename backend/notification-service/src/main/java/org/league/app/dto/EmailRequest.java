ccddba19ca1b1f41e981e9b67c689b1cce8a6f4c
Yaroslav Lopatkin <ylopatkin@gmail.com>
2024-11-29 17:22:45 +0100

    29.11.2024 - 35-email-confirmation | added EmailRequest entity for handling email requests

---
+++ backend/notification-service/src/main/java/org/league/app/dto/EmailRequest.java
@@ -0,0 +1,15 @@
+package org.league.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailRequest {
+    private String to;
+    private String subject;
+    private String body;
+}
+
