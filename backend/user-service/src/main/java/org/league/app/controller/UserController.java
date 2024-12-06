5ebaf14be3d8a89bb1e40ab4026414e42d11e7a3
Yaroslav Lopatkin <ylopatkin@gmail.com>
2024-12-06 19:51:18 +0100

    06.12.2024 - 58-implement-JWT-authentication | split the controller into two separate

--- backend/user-service/src/main/java/org/league/app/controller/UserController.java
+++ backend/user-service/src/main/java/org/league/app/controller/UserController.java
@@ -2,16 +2,8 @@ package org.league.app.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.league.app.database.repository.UserRepository;
-import org.league.app.dto.UserCreateEditDto;
-import org.league.app.dto.UserReadDto;
-import org.league.app.service.UserService;
-import org.league.app.validation.CreateAction;
-import org.league.app.validation.EditAction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -20,35 +12,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/user")
 public class UserController {
 
-    private final UserService userService;
-    private final UserRepository userRepository;
 
-    @PostMapping("/registration")
-    public ResponseEntity<UserReadDto> registration(@RequestBody @Validated({CreateAction.class, EditAction.class}) UserCreateEditDto userCreateEditDto, BindingResult bindingResult){
-        if(userRepository.existsByEmail(userCreateEditDto.getEmail()) && userRepository.existsByUsername(userCreateEditDto.getUsername())){
-            log.error("'{}' is already registered",userCreateEditDto.getEmail());
-            log.error("'{}' is already in use",userCreateEditDto.getUsername());
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
-        }
-
-        if(userRepository.existsByEmail(userCreateEditDto.getEmail()) || userRepository.existsByUsername(userCreateEditDto.getUsername())){
-            log.error("'{}' is already exist",
-                    userRepository.existsByEmail(userCreateEditDto.getEmail()) ? userCreateEditDto.getEmail() : userCreateEditDto.getUsername());
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
-        }
-
-        if(bindingResult.hasErrors()){
-            bindingResult.getFieldErrors().forEach(fieldError -> log.error(fieldError.getField() + ": " + fieldError.getDefaultMessage()));
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        var userReadDto = userService.create(userCreateEditDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userReadDto);
+    @GetMapping("/test")
+    public ResponseEntity<String> test(){
+        return ResponseEntity.status(HttpStatus.OK).body("OK");
     }
 
-    @GetMapping("/activate")
-    public ResponseEntity<Boolean> activate(@RequestParam("token") String token){
-        boolean confirmation = userService.emailConfirmation(token);
-        log.info("'{}' is activated",token);
-        return ResponseEntity.ok(confirmation);
+    @GetMapping("/test2")
+    public ResponseEntity<String> test2(){
+        return ResponseEntity.status(HttpStatus.OK).body("OK");
     }
 }
