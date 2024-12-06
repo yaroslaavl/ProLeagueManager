6d656caca55b3a0936fe0351814fc04044a32ee5
Yaroslav Lopatkin <ylopatkin@gmail.com>
2024-12-06 19:52:41 +0100

    06.12.2024 - 58-implement-JWT-authentication | split the controller into two

---
+++ backend/user-service/src/main/java/org/league/app/controller/AuthController.java
@@ -0,0 +1,95 @@
+package org.league.app.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.league.app.database.repository.UserRepository;
+import org.league.app.dto.AuthResponseDto;
+import org.league.app.dto.LoginDto;
+import org.league.app.dto.UserCreateEditDto;
+import org.league.app.dto.UserReadDto;
+import org.league.app.service.JWTService;
+import org.league.app.service.UserService;
+import org.league.app.validation.CreateAction;
+import org.league.app.validation.EditAction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    private final JWTService jwtService;
+
+    private final UserService userService;
+
+    private final UserRepository userRepository;
+
+    @PostMapping("/registration")
+    public ResponseEntity<UserReadDto> registration(@RequestBody @Validated({CreateAction.class, EditAction.class}) UserCreateEditDto userCreateEditDto, BindingResult bindingResult){
+        if(userRepository.existsByEmail(userCreateEditDto.getEmail()) && userRepository.existsByUsername(userCreateEditDto.getUsername())){
+            log.error("'{}' is already registered",userCreateEditDto.getEmail());
+            log.error("'{}' is already in use",userCreateEditDto.getUsername());
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        if(userRepository.existsByEmail(userCreateEditDto.getEmail()) || userRepository.existsByUsername(userCreateEditDto.getUsername())){
+            log.error("'{}' is already exist",
+                    userRepository.existsByEmail(userCreateEditDto.getEmail()) ? userCreateEditDto.getEmail() : userCreateEditDto.getUsername());
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        if(bindingResult.hasErrors()){
+            bindingResult.getFieldErrors().forEach(fieldError -> log.error(fieldError.getField() + ": " + fieldError.getDefaultMessage()));
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        var userReadDto = userService.create(userCreateEditDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userReadDto);
+    }
+
+    @PostMapping("/login")
+    public AuthResponseDto login(@RequestBody LoginDto loginDto){
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginDto.getUsername(),
+                loginDto.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authenticate);
+        String accessToken = jwtService.generateToken(authenticate);
+        String refreshToken = jwtService.generateRefreshToken(authenticate);
+        log.info("accessToken token: {}", accessToken);
+        log.info("refreshToken token: {}", refreshToken);
+        return AuthResponseDto.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(HttpServletResponse response, HttpServletRequest request) throws IOException {
+        jwtService.generateRefreshToken(request,response);
+    }
+
+    @GetMapping("/activate")
+    public ResponseEntity<Boolean> activate(@RequestParam("token") String token){
+        boolean confirmation = userService.emailConfirmation(token);
+        log.info("'{}' is activated",token);
+        return ResponseEntity.ok(confirmation);
+    }
+}
