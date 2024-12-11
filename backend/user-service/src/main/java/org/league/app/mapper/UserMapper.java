c15aae57ca64975c90db0747146a15dc8b87c858
Yaroslav Lopatkin <ylopatkin@gmail.com>
2024-12-11 20:20:24 +0100

    11.12.2024 - 68-create-a-panel-for-the-user -to-manage-his-account | added toPublicProfileDto in mapper

--- backend/user-service/src/main/java/org/league/app/mapper/UserMapper.java
+++ backend/user-service/src/main/java/org/league/app/mapper/UserMapper.java
@@ -1,7 +1,8 @@
 package org.league.app.mapper;
 
 import org.league.app.database.entity.User;
-import org.league.app.dto.UserCreateEditDto;
+import org.league.app.dto.UserCreateDto;
+import org.league.app.dto.UserPublicProfileDto;
 import org.league.app.dto.UserReadDto;
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
@@ -15,7 +16,9 @@ public interface UserMapper {
     @Mappings({
             @Mapping(target = "password", expression = "java(passwordEncoder.encode(userCreateEditDto.getPassword()))"),
     })
-    User toEntity(UserCreateEditDto userCreateEditDto, @Context PasswordEncoder passwordEncoder);
+    User toEntity(UserCreateDto userCreateEditDto, @Context PasswordEncoder passwordEncoder);
 
     UserReadDto toDto(User user);
+
+    UserPublicProfileDto toPublicProfileDto(User user);
 }
