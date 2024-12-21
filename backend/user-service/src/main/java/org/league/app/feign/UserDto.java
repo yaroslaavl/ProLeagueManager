9169b784c0e935767eff0c91d4458aa76fcd2d78
Yaroslav Lopatkin <ylopatkin@gmail.com>
2024-12-21 18:55:40 +0100

    21.12.2024 - 76-build-backend-for-team-management-system | added dto for feign

--- backend/user-service/src/main/java/org/league/app/feign/UserDto.java
+++ backend/user-service/src/main/java/org/league/app/feign/UserDto.java
@@ -1,15 +1,19 @@
 package org.league.app.feign;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
 
+    private Long id;
     private String email;
-    private String username;
     private List<String> roles;
 }
