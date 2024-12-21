ce43da00197af4c2fdb7786b31649186ba3e5a8f
Yaroslav Lopatkin <ylopatkin@gmail.com>
2024-12-21 19:02:46 +0100

    21.12.2024 - 76-build-backend-for-team-management-system | added create team method

---
+++ backend/team-management-service/src/main/java/org/league/app/service/TeamService.java
@@ -0,0 +1,71 @@
+package org.league.app.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.league.app.database.entity.Team;
+import org.league.app.database.entity.TeamMember;
+import org.league.app.database.entity.enums.TeamRole;
+import org.league.app.database.entity.enums.TeamStatus;
+import org.league.app.database.repository.TeamMemberRepository;
+import org.league.app.database.repository.TeamRepository;
+import org.league.app.dto.TeamCreateEditDto;
+import org.league.app.dto.TeamReadDto;
+import org.league.app.exception.TeamAlreadyExistsException;
+import org.league.app.feign.AuthClientFeign;
+import org.league.app.feign.UserDto;
+import org.league.app.mapper.TeamMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TeamService {
+
+    private final TeamMemberRepository teamMemberRepository;
+    private final TeamRepository teamRepository;
+    private final TeamMapper teamMapper;
+    private final AuthClientFeign authClientFeign;
+
+    @Transactional
+    public TeamReadDto createTeam(TeamCreateEditDto teamCreateDto) {
+        UserDto userByEmail = authClientFeign.getUserByEmail(securityContext());
+
+        Optional<Team> existingTeam = teamRepository.findTeamByTeamName(teamCreateDto.getTeamName());
+        if (existingTeam.isPresent()) {
+            throw new TeamAlreadyExistsException("Team name already exists");
+        }
+
+        Team team = Optional.of(teamCreateDto)
+                .map(dto -> {
+                    Team entity = teamMapper.toEntity(dto);
+                    entity.setTeamStatus(TeamStatus.INACTIVE);
+                    return teamRepository.saveAndFlush(entity);
+                }).orElseThrow();
+
+        TeamMember teamMember = TeamMember.builder()
+                .team(team)
+                .userId(userByEmail.getId())
+                .teamRole(TeamRole.MANAGER)
+                .isSubstitute(false)
+                .build();
+
+        teamRepository.saveAndFlush(team);
+        teamMemberRepository.save(teamMember);
+
+        return teamMapper.toDto(team);
+    }
+
+    private String securityContext() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getPrincipal() instanceof UserDto) {
+            UserDto userDto = (UserDto) authentication.getPrincipal();
+            return userDto.getEmail();
+        }
+        return authentication.getName();
+    }
+
+}
