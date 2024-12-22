1e05ba8dff5a206dfe00167d98e0e9f77ce7cc03
Yaroslav Lopatkin <ylopatkin@gmail.com>
2024-12-22 15:44:01 +0100

    21.12.2024 - 76-build-backend-for-team-management-system | fixed a bug by adding @Repository

--- backend/user-service/src/main/java/org/league/app/database/repository/RoleGroupRepository.java
+++ backend/user-service/src/main/java/org/league/app/database/repository/RoleGroupRepository.java
@@ -4,9 +4,11 @@ import org.league.app.database.entity.RoleGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleGroupRepository extends JpaRepository<RoleGroup, Integer> {
 
     Optional<RoleGroup> findByName(String name);
