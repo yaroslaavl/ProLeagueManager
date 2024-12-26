4930328916f6f9f7fc6d211a6c8cc39732550a2f
Yaroslav Lopatkin <ylopatkin@gmail.com>
2024-12-26 18:37:34 +0100

    26.12.2024 - 76-build-backend-for-team-management-system | NOTIFICATION-SERVICE | created Enums

---
+++ backend/notification-service/src/main/java/org/league/app/database/entity/enums/EventType.java
@@ -0,0 +1,13 @@
+package org.league.app.database.entity.enums;
+
+public enum EventType {
+
+    TEAM_INVITE,
+    TEAM_ACCEPT,
+    TEAM_REJECT,
+    TEAM_CANCELLED,
+    TEAM_JOINED,
+    TEAM_LEFT,
+    TEAM_CREATED,
+    TEAM_DELETED,
+}
