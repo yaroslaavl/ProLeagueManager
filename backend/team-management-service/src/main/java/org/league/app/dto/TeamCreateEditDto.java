39904654e506122accb32b292ed553e7a7c7547c
Yaroslav Lopatkin <ylopatkin@gmail.com>
2024-12-26 18:28:05 +0100

    26.12.2024 - 76-build-backend-for-team-management-system | added validation to the field

--- backend/team-management-service/src/main/java/org/league/app/dto/TeamCreateEditDto.java
+++ backend/team-management-service/src/main/java/org/league/app/dto/TeamCreateEditDto.java
@@ -1,10 +1,17 @@
 package org.league.app.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.league.app.validation.CreateAction;
+import org.league.app.validation.EditAction;
 
 @Data
 public class TeamCreateEditDto {
 
-    String teamName;
-
+    @NotBlank(message = "Team name must not be blank", groups = {CreateAction.class, EditAction.class})
+    @Size(min = 3, max = 50, message = "Team name must be between 3 and 50 characters", groups = {CreateAction.class, EditAction.class})
+    @Pattern(regexp = "^[a-zA-Z0-9 ]+$", message = "Team name can only contain alphanumeric characters and spaces", groups = {CreateAction.class, EditAction.class})
+    private String teamName;
 }
