630043cdfaca3fe540b3bbbdde4b8d7a2e8cbc23
Yaroslav Lopatkin <ylopatkin@gmail.com>
2024-12-26 18:36:14 +0100

    26.12.2024 - 76-build-backend-for-team-management-system | NOTIFICATION-SERVICE | added @EnableFeignClients

--- backend/notification-service/src/main/java/org/league/app/NotificationServiceApplication.java
+++ backend/notification-service/src/main/java/org/league/app/NotificationServiceApplication.java
@@ -4,9 +4,11 @@ package org.league.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NotificationServiceApplication {
