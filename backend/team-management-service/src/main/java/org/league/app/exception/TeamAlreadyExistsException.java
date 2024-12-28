88e805f3fc0ae1faf1db3d613312e5b078c8baf7
Yaroslav Lopatkin <ylopatkin@gmail.com>
2024-12-28 03:23:56 +0100

    28.12.2024 - feature/107-global-exception-handler | removed 1 custom exception and added 2 another

--- backend/team-management-service/src/main/java/org/league/app/exception/TeamAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package org.league.app.exception;
-
-public class TeamAlreadyExistsException extends RuntimeException {
-    public TeamAlreadyExistsException(String teamNameAlreadyExists) {
-        super(teamNameAlreadyExists);
-    }
-}
