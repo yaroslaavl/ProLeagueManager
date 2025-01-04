bcc0bb2bcd2f19581089dfc0c18d3710a4f050bf
Yaroslav Lopatkin <ylopatkin@gmail.com>
2025-01-04 22:02:58 +0100

    04.01.2025 - feature/106-send-message-team-invitation-if-user-is-subscribed | NOTIFICATION-SERVICE | added cors config

---
+++ backend/notification-service/src/main/java/org/league/app/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package org.league.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:63342")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
