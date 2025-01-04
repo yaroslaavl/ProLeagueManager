e5e541a760f886826bb60b8ed81f761f2319990f
Yaroslav Lopatkin <ylopatkin@gmail.com>
2025-01-04 22:14:09 +0100

    04.01.2025 - feature/106-send-message-team-invitation-if-user-is-subscribed | added new Table and connected it to TeamMember

---
+++ backend/team-management-service/src/main/java/org/league/app/database/entity/TeamRole.java
@@ -0,0 +1,32 @@
+package org.league.app.database.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Table(schema = "public", name = "team_role")
+public class TeamRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false, name = "name")
+    private String name;
+
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
