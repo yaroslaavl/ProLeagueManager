4a98ca91b29d7ae2e6b0de0c3e10929f0713d97d
Yaroslav Lopatkin <ylopatkin@gmail.com>
2025-01-04 22:05:56 +0100

    04.01.2025 - feature/106-send-message-team-invitation-if-user-is-subscribed | NOTIFICATION-SERVICE | added new custom exception

---
+++ backend/notification-service/src/main/java/org/league/app/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package org.league.app.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
