79a8a111c323ec4084dea455c01c465d598e7bef
Ivan Petrov <huko2990@gmail.com>
2025-01-08 00:44:32 +0100

    8.01.2025 - 114-create-connection-to-backend | created logout mathod

---
+++ frontend/js/manage.js
@@ -0,0 +1,19 @@
+async function logOut(){
+  try {
+    const response = await fetch('http://localhost:8765/auth/logout',{
+      method: 'POST',
+      headers: {
+        "Authorization": `Bearer ${accToken}`, // Добавляем заголовок Authorization
+        "Content-Type": "application/json" // Указываем формат данных
+      }
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    localStorage.clear();
+    window.location.href = "main.html";
+  }catch (err){
+    console.error(`${err}`);
+  }
+}
+const logOutBtn = document.getElementById('log-out').addEventListener('click',logOut);
