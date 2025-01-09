9a7633b36695e3126de2e3177f1bbcf6b2f95068
Yaroslav Lopatkin <ylopatkin@gmail.com>
2025-01-10 00:03:52 +0100

    10.01.2025 - feature/132-build-competition-main-logic | added Game System config

---
+++ backend/competition-management-service/src/main/java/org/league/app/config/GameSystemConfig.java
@@ -0,0 +1,36 @@
+package org.league.app.config;
+
+import lombok.SneakyThrows;
+import org.league.app.filter.JWTFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class GameSystemConfig {
+
+    private final JWTFilter jwtFilter;
+
+    public GameSystemConfig(JWTFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    @SneakyThrows
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/api/game-system/create-game-system", "/api/game-system/get-system-by-name").hasAuthority("EDITOR"))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return httpSecurity.build();
+    }
+}
