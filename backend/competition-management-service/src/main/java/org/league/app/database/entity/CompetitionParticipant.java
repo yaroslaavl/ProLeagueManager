519e64597cd95dbfb7fa5f503a71127d589d2743
Pon4ik123 <taranenkozahar@gmail.com>
2025-01-10 00:33:22 +0100

    10.01.2025 - feature/132-build-competition-main-logic | added Entities

---
+++ backend/competition-management-service/src/main/java/org/league/app/database/entity/CompetitionParticipant.java
@@ -0,0 +1,57 @@
+package org.league.app.database.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.league.app.database.entity.enums.CompetitionParticipantStatus;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Table(schema = "public", name = "competition_participant")
+public class CompetitionParticipant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "competition_id", nullable = false)
+    private Competition competition;
+
+    @Column(name = "team_id")
+    private UUID teamId;
+
+    @Column(name = "player_id")
+    private Long playerId;
+
+    @Column(name = "is_team")
+    private Boolean isTeam;
+
+    @Column(name = "points")
+    private Integer points;
+
+    @Column(name = "registered_at", nullable = false, updatable = false)
+    private OffsetDateTime registeredAt;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private CompetitionParticipantStatus competitionParticipantStatus;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private OffsetDateTime updatedAt;
+}
---
+++ backend/competition-management-service/src/main/java/org/league/app/database/entity/CompetitionStage.java
@@ -0,0 +1,43 @@
+package org.league.app.database.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Table(schema = "public", name = "competition_stage")
+public class CompetitionStage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "competition_id", nullable = false)
+    private Competition competition;
+
+    @Column(name = "stage_name")
+    private String stageName;
+
+    @Column(name = "is_elimination")
+    private Boolean isElimination;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
