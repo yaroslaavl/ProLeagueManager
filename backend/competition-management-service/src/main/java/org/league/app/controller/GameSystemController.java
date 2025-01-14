22c381d69fbc23b121f8d0c39f3a49f74469281b
yaroslaavl <ylopatkin@gmail.com>
2025-01-14 23:50:14 +0100

    14.01.2025 - feature/132-build-competition-main-logic | added delete and search endpoints

--- backend/competition-management-service/src/main/java/org/league/app/controller/GameSystemController.java
+++ backend/competition-management-service/src/main/java/org/league/app/controller/GameSystemController.java
@@ -1,11 +1,14 @@
 package org.league.app.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.league.app.dto.GameSystemCreateCompetitionDto;
 import org.league.app.dto.GameSystemCreateEditDto;
 import org.league.app.dto.GameSystemReadDto;
 import org.league.app.service.GameSystemService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/game-system")
@@ -24,4 +27,19 @@ public class GameSystemController {
         GameSystemReadDto gameSystem = gameSystemService.getGameSystemById(id);
         return ResponseEntity.ok(gameSystem);
     }
+
+    @DeleteMapping("/delete-game-system/{id}")
+    public ResponseEntity<?> deleteGameSystem(@PathVariable("id") Integer id) {
+        gameSystemService.deleteGameSystemById(id);
+        return ResponseEntity.ok("Deleted GameSystem");
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<GameSystemCreateCompetitionDto>> searchGameSystem(@RequestParam("query") String query) {
+        List<GameSystemCreateCompetitionDto> gameSystemCreateCompetitionDtoList =
+                gameSystemService.searchGameSystem(query);
+
+        return ResponseEntity.ok(gameSystemCreateCompetitionDtoList);
+    }
+
 }
