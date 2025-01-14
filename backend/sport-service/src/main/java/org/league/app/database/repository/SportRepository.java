5f1b71454cc380a43e99217b9385d86fccf61d23
yaroslaavl <ylopatkin@gmail.com>
2025-01-14 23:37:32 +0100

    14.01.2025 - feature/132-build-competition-main-logic | added new sport custom dynamic search method for game system service

--- backend/sport-service/src/main/java/org/league/app/database/repository/SportRepository.java
+++ backend/sport-service/src/main/java/org/league/app/database/repository/SportRepository.java
@@ -2,8 +2,11 @@ package org.league.app.database.repository;
 
 import org.league.app.database.entity.Sport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +16,6 @@ public interface SportRepository extends JpaRepository<Sport, Integer> {
 
     int deleteSportByName(String name);
 
+    @Query("SELECT s FROM Sport s WHERE LOWER(s.name) LIKE LOWER(CONCAT(:sportName, '%'))")
+    List<Sport> findByNameSearch(@Param("sportName") String sportName);
 }
