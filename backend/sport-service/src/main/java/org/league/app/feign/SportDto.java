9aebf11e59b72bb128e84be3208f2d14d8b470f0
yaroslaavl <ylopatkin@gmail.com>
2025-01-14 23:38:33 +0100

    14.01.2025 - feature/132-build-competition-main-logic | added Dto for communication between sports and game system services

---
+++ backend/sport-service/src/main/java/org/league/app/feign/SportDto.java
@@ -0,0 +1,16 @@
+package org.league.app.feign;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SportDto {
+
+    private Integer id;
+    private String name;
+}
