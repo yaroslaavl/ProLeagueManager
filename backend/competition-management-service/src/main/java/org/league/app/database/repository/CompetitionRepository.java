90a30b5d7353a3ad6e87671356c697f9980bb408
Pon4ik123 <taranenkozahar@gmail.com>
2025-01-14 23:27:09 +0100

    14.01.2025 - feature/132-build-competition-main-logic | added repository method

--- backend/competition-management-service/src/main/java/org/league/app/database/repository/CompetitionRepository.java
+++ backend/competition-management-service/src/main/java/org/league/app/database/repository/CompetitionRepository.java
@@ -4,9 +4,12 @@ import org.league.app.database.entity.Competition;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CompetitionRepository extends JpaRepository<Competition, UUID> {
+
+    Optional<Competition> findCompetitionByName(String competitionName);
 
 }
