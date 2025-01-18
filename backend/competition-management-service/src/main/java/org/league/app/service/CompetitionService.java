368fc7eafda5d2398fc601d4e4f070b3d35d2e02
Pon4ik123 <taranenkozahar@gmail.com>
2025-01-19 00:40:48 +0100

    13.01.2025 - feature/136-CRUD-competition-operations' | added service methods to delete and update competition

--- backend/competition-management-service/src/main/java/org/league/app/service/CompetitionService.java
+++ backend/competition-management-service/src/main/java/org/league/app/service/CompetitionService.java
@@ -9,18 +9,20 @@ import org.league.app.database.repository.GameSystemRepository;
 import org.league.app.dto.CompetitionCreateEditDto;
 import org.league.app.dto.CompetitionReadDto;
 import org.league.app.exception.CompetitionAlreadyExists;
+import org.league.app.exception.CompetitionNotFoundException;
 import org.league.app.exception.GameSystemNotFoundException;
 import org.league.app.mapper.CompetitionMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class CompetitionService {
+public class    CompetitionService {
 
     private final CompetitionRepository competitionRepository;
     private final GameSystemRepository gameSystemRepository;
@@ -51,5 +53,35 @@ public class CompetitionService {
 
         competitionRepository.save(competition);
         return competitionMapper.toDto(competition);
+    }
+
+    @Transactional
+    public CompetitionReadDto edit(String competitionName, CompetitionCreateEditDto newCompetition) {
+        Competition currentCompetition = competitionRepository.findCompetitionByName(competitionName)
+                .orElseThrow(() -> new CompetitionNotFoundException("Competition not found : " + competitionName));
+
+        if (competitionRepository.findCompetitionByName(newCompetition.getName()).isPresent()) {
+            throw new CompetitionAlreadyExists("Competition with name:" + newCompetition.getName() + " already exists");
+        }
+
+        Optional.ofNullable(newCompetition.getName()).ifPresent(currentCompetition::setName);
+        Optional.ofNullable(newCompetition.getSportId()).ifPresent(currentCompetition::setSportId);
+        Optional.ofNullable(newCompetition.getCompetitionType()).map(CompetitionType::valueOf).ifPresent(currentCompetition::setCompetitionType);
+        Optional.ofNullable(newCompetition.getStartDate()).ifPresent(currentCompetition::setStartDate);
+        Optional.ofNullable(newCompetition.getEndDate()).ifPresent(currentCompetition::setEndDate);
+
+        competitionRepository.save(currentCompetition);
+        return competitionMapper.toDto(currentCompetition);
+    }
+
+    @Transactional
+    public boolean delete(String competitionName) {
+        return competitionRepository.findCompetitionByName(competitionName)
+                .map(entity -> {
+                    int deleted = competitionRepository.deleteCompetitionByName(competitionName.trim());
+                    competitionRepository.flush();
+                    return deleted > 0;
+                })
+                .orElse(false);
     }
 }
