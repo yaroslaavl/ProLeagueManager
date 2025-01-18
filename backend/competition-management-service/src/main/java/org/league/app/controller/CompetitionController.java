56506a9e1dad1ddc940f6d47e93171fedeaa846a
Pon4ik123 <taranenkozahar@gmail.com>
2025-01-19 00:39:42 +0100

    13.01.2025 - feature/136-CRUD-competition-operations' | added update and delete methods for competition

--- backend/competition-management-service/src/main/java/org/league/app/controller/CompetitionController.java
+++ backend/competition-management-service/src/main/java/org/league/app/controller/CompetitionController.java
@@ -7,6 +7,9 @@ import org.league.app.service.CompetitionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.ResponseEntity.noContent;
+import static org.springframework.http.ResponseEntity.notFound;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/competition")
@@ -20,4 +23,18 @@ public class CompetitionController {
                                                                  @RequestParam("sportId") Integer sportId) {
         return ResponseEntity.ok(competitionService.createCompetition(competitionCreate, gameSystemId, sportId));
     }
+
+    @PutMapping("/edit-competition/{competitionName}")
+    public ResponseEntity<CompetitionReadDto> editCompetition (@PathVariable("competitionName") String competitionName,
+                                                               @RequestBody CompetitionCreateEditDto newCompetition) {
+        return ResponseEntity.ok(competitionService.edit(competitionName, newCompetition));
+    }
+
+    @DeleteMapping("/delete-competition")
+    public ResponseEntity<?> deleteCompetition (@RequestParam("competitionName") String competitionName) {
+        return competitionService.delete(competitionName)
+                ? noContent().build()
+                : notFound().build();
+    }
+
 }
