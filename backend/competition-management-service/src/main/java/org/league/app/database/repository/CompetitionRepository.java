bd94c74d7ab8b2c9de424480c79339f25d363390
Pon4ik123 <taranenkozahar@gmail.com>
2025-01-19 00:40:16 +0100

    13.01.2025 - feature/136-CRUD-competition-operations' | added repository method to delete competition

--- backend/competition-management-service/src/main/java/org/league/app/database/repository/CompetitionRepository.java
+++ backend/competition-management-service/src/main/java/org/league/app/database/repository/CompetitionRepository.java
@@ -12,4 +12,6 @@ public interface CompetitionRepository extends JpaRepository<Competition, UUID> 
 
     Optional<Competition> findCompetitionByName(String competitionName);
 
+    int deleteCompetitionByName(String competitionName);
+
 }
