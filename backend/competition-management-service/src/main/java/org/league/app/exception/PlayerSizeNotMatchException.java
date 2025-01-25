fa452f4380158ff3c077510bb4f2d66034a3a454
yaroslaavl <ylopatkin@gmail.com>
2025-01-25 11:43:35 +0100

    25.01.2025 - feature/132-build-competition-main-logic | added new custom exception

---
+++ backend/competition-management-service/src/main/java/org/league/app/exception/PlayerSizeNotMatchException.java
@@ -0,0 +1,7 @@
+package org.league.app.exception;
+
+public class PlayerSizeNotMatchException extends RuntimeException {
+    public PlayerSizeNotMatchException(String message) {
+        super(message);
+    }
+}
