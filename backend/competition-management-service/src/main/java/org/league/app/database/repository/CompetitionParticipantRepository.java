d3a9dfed407e9864b4cf549dc7f21753424123fc
yaroslaavl <ylopatkin@gmail.com>
2025-01-31 22:20:00 +0100

    31.01.2025 - feature/139-implement-team-notification-after-joining-the-competition | added repository methods

--- backend/competition-management-service/src/main/java/org/league/app/database/repository/CompetitionParticipantRepository.java
+++ backend/competition-management-service/src/main/java/org/league/app/database/repository/CompetitionParticipantRepository.java
@@ -4,9 +4,13 @@ import org.league.app.database.entity.CompetitionParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CompetitionParticipantRepository extends JpaRepository<CompetitionParticipant, UUID> {
 
+    Optional<CompetitionParticipant> findCompetitionParticipantByTeamIdAndCompetitionId(UUID id, UUID competitionId);
+
+    Optional<CompetitionParticipant> findCompetitionParticipantByPlayerIdAndCompetitionId(Long id, UUID competitionId);
 }
