e6275b53dbb737cc8a5ebf3a19fb9d2786ded43e
Pon4ik123 <taranenkozahar@gmail.com>
2025-02-01 19:30:34 +0100

    01.02.2025 - feature/138-error-and-exception-handling | renamed exception

--- backend/competition-management-service/src/main/java/org/league/app/exception/CompetitionAlreadyExists.java
+++
@@ -1,7 +0,0 @@
-package org.league.app.exception;
-
-public class CompetitionAlreadyExists extends RuntimeException {
-    public CompetitionAlreadyExists(String message) {
-        super(message);
-    }
-}
