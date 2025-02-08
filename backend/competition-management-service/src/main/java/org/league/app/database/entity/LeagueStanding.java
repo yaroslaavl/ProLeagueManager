e3f8f45206a6231f34e90c2cc3730f57e5fd07e8
Pon4ik123 <taranenkozahar@gmail.com>
2025-02-08 15:18:20 +0100

    08.02.2025 - feature/154-add-league-stage-entity | created LeagueStanding entity

---
+++ backend/competition-management-service/src/main/java/org/league/app/database/entity/LeagueStanding.java
@@ -0,0 +1,55 @@
+package org.league.app.database.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@Table(name = "league_standings")
+public class LeagueStanding {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "competition_id", nullable = false)
+    private Competition competition;
+
+    @Column(name = "team_id")
+    private UUID teamId;
+
+    @Column(name = "player_id")
+    private Long playerId;
+
+    @Column(name = "wins")
+    private Integer wins;
+
+    @Column(name = "losses")
+    private Integer losses;
+
+    @Column(name = "draws")
+    private Integer draws;
+
+    @Column(name = "points")
+    private Integer points;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
