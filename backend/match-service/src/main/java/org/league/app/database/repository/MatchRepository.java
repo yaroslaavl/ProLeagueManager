80e3edbf85f81f1db12ec55d504995a2fb6fcc2c
Pon4ik123 <taranenkozahar@gmail.com>
2025-02-08 16:18:08 +0100

    08.02.2025 - feature/157-add-match-entity | created repository for Match

---
+++ backend/match-service/src/main/java/org/league/app/database/repository/MatchRepository.java
@@ -0,0 +1,12 @@
+package org.league.app.database.repository;
+
+import org.league.app.database.entity.Match;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MatchRepository extends JpaRepository<Match, UUID> {
+
+}
