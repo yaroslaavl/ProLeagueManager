2435093169ff3684f91ca1634e903c4c32bc8a6e
yaroslaavl <ylopatkin@gmail.com>
2025-02-08 17:37:44 +0100

    08.02.2025 - feature/155-add-tournament-stage-generation | renamed repository

--- backend/competition-management-service/src/main/java/org/league/app/database/repository/TournamentStageRepository.java
+++ backend/competition-management-service/src/main/java/org/league/app/database/repository/TournamentStageRepository.java
@@ -1,12 +1,12 @@
 package org.league.app.database.repository;
 
-import org.league.app.database.entity.CompetitionStage;
+import org.league.app.database.entity.TournamentStage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface CompetitionStageRepository extends JpaRepository<CompetitionStage, UUID> {
+public interface TournamentStageRepository extends JpaRepository<TournamentStage, UUID> {
 
 }
