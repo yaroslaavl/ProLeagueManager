75f12e3035cc8d58323eff0394281143edbadffd
Pon4ik123 <taranenkozahar@gmail.com>
2025-02-08 16:17:53 +0100

    08.02.2025 - feature/157-add-match-entity | created MatchPlayer entity

---
+++ backend/match-service/src/main/java/org/league/app/database/entity/MatchPlayer.java
@@ -0,0 +1,46 @@
+package org.league.app.database.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@Table(schema = "public", name = "match_player")
+public class MatchPlayer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "match_id", nullable = false)
+    private Match match;
+
+    @Column(name = "player_id")
+    private Long playerId;
+
+    @Column(name = "team_id")
+    private UUID teamId;
+
+    @Column(name = "is_starting")
+    private Boolean isStarting;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
