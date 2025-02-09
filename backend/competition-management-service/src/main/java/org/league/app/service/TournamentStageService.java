741519dcba05fed344497b5957de6a4dfd469636
yaroslaavl <ylopatkin@gmail.com>
2025-02-09 16:51:30 +0100

    09.02.2025 - feature/158-change-tournament-status-logic | Implemented a scheduled task to automatically check and start tournaments and Added logic to close tournament registration and generate stages once a tournament starts

--- backend/competition-management-service/src/main/java/org/league/app/service/TournamentStageService.java
+++ backend/competition-management-service/src/main/java/org/league/app/service/TournamentStageService.java
@@ -5,30 +5,87 @@ import lombok.extern.slf4j.Slf4j;
 import org.league.app.database.entity.Competition;
 import org.league.app.database.entity.TournamentStage;
 import org.league.app.database.entity.enums.CompetitionStatus;
+import org.league.app.database.entity.enums.CompetitionType;
+import org.league.app.database.repository.CompetitionParticipantRepository;
 import org.league.app.database.repository.CompetitionRepository;
 import org.league.app.database.repository.TournamentStageRepository;
+import org.league.app.exception.CompetitionNotFoundException;
 import org.league.app.exception.IncorrectTournamentStatus;
+import org.league.app.feign.sportClient.SportClientFeign;
+import org.league.app.feign.sportClient.SportDto;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class TournamentStageService {
 
+    private final CompetitionParticipantRepository competitionParticipantRepository;
     private final TournamentStageRepository tournamentStageRepository;
     private final CompetitionRepository competitionRepository;
+    private final SportClientFeign sportClientFeign;
 
     @Transactional
-    public void generateTournamentStages(UUID competitionId) {
+    public void closeTournamentRegistrationAndGenerateTournamentStages(UUID competitionId) {
         Competition competition = competitionRepository.findById(competitionId)
-                .orElseThrow(() -> new IllegalArgumentException("Competition not found"));
+                .orElseThrow(() -> new CompetitionNotFoundException("Competition not found"));
+
+        if (checkTournamentCapacity(competitionId)) {
+            competition.setStatus(CompetitionStatus.CANCELLED);
+        } else {
+            competition.setStatus(CompetitionStatus.ACTIVE);
+        }
+
+        competitionRepository.saveAndFlush(competition);
+
+        if (competition.getStatus().equals(CompetitionStatus.ACTIVE)) {
+            generateTournamentStages(competitionId);
+        }
+    }
+
+    @Scheduled(fixedDelay = 60000)
+    public void autoStartTournaments() {
+        log.info("Checking tournaments at {}", LocalDateTime.now());
+
+        List<Competition> competitions = competitionRepository.findAllByStatusAndCompetitionType(CompetitionStatus.UPCOMING, CompetitionType.TOURNAMENT);
+
+        for (Competition competition : competitions) {
+            SportDto sportDto = sportClientFeign.findSportById(competition.getSportId());
+            try {
+                if (sportDto.getIsEsport()) {
+                    if (LocalDateTime.now().isAfter(competition.getStartDate().minusHours(1))) {
+                        log.info("Auto-starting e-sport tournament: '{}'", competition.getName());
+                        closeTournamentRegistrationAndGenerateTournamentStages(competition.getId());
+                    }
+                } else {
+                    if (LocalDateTime.now().isAfter(competition.getStartDate().minusHours(24))) {
+                        log.info("Auto-starting sport tournament: '{}'", competition.getName());
+                        closeTournamentRegistrationAndGenerateTournamentStages(competition.getId());
+                    }
+                }
+            } catch (Exception e) {
+                log.error("Failed to start tournament '{}': {}", competition.getName(), e.getMessage(), e);
+            }
+        }
+    }
+
+    private void generateTournamentStages(UUID competitionId) {
+        Competition competition = competitionRepository.findById(competitionId)
+                .orElseThrow(() -> new CompetitionNotFoundException("Competition not found"));
 
         if (!competition.getStatus().equals(CompetitionStatus.ACTIVE)) {
             throw new IncorrectTournamentStatus("Competition is not active");
+        }
+
+        if (tournamentStageRepository.findTournamentStagesByCompetition(competition).isPresent()) {
+            log.error("Tournament stages already exist in competition '{}'", competition.getName());
+            return;
         }
 
         Integer maxTeamSize = competition.getGameSystem().getMaxTeamSize();
@@ -42,6 +99,7 @@ public class TournamentStageService {
             } else {
                 tempStage = "1/" + i;
             }
+
             String rules = competition.getGameSystem().getRules();
             boolean isElimination = rules.startsWith("Single Elimination");
             TournamentStage tournamentStage = TournamentStage.builder()
@@ -54,5 +112,13 @@ public class TournamentStageService {
             tournamentStageRepository.saveAndFlush(tournamentStage);
             counter++;
         }
+    }
+
+    private boolean checkTournamentCapacity(UUID competitionId) {
+        Competition competitionWithGameSystem = competitionRepository.findById(competitionId)
+                .orElseThrow(() -> new CompetitionNotFoundException("Competition not found"));
+
+        return competitionWithGameSystem.getGameSystem().getMinTeamSize() >
+                competitionParticipantRepository.countTeamsOrUsersByCompetitionId(competitionId);
     }
 }
