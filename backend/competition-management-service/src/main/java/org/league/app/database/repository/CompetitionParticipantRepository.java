63ddd788b98afb2a54908f1ddd30742c218b5df3
yaroslaavl <ylopatkin@gmail.com>
2025-02-11 16:26:16 +0100

    11.02.2025 - feature/156-add-tournament-match-generation | updated repository methods

--- backend/competition-management-service/src/main/java/org/league/app/database/repository/CompetitionParticipantRepository.java
+++ backend/competition-management-service/src/main/java/org/league/app/database/repository/CompetitionParticipantRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -24,5 +25,7 @@ public interface CompetitionParticipantRepository extends JpaRepository<Competit
     Optional<CompetitionParticipant> findCompetitionParticipantByTeamIdAndCompetitionId(UUID id, UUID competitionId);
 
     Optional<CompetitionParticipant> findCompetitionParticipantByPlayerIdAndCompetitionId(Long id, UUID competitionId);
+
+    List<CompetitionParticipant> findAllByCompetitionId(UUID competitionId);
 
 }
--- backend/competition-management-service/src/main/java/org/league/app/database/repository/TournamentStageRepository.java
+++ backend/competition-management-service/src/main/java/org/league/app/database/repository/TournamentStageRepository.java
@@ -1,16 +1,14 @@
 package org.league.app.database.repository;
 
-import org.league.app.database.entity.Competition;
 import org.league.app.database.entity.TournamentStage;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.expression.spel.ast.OpAnd;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TournamentStageRepository extends JpaRepository<TournamentStage, UUID> {
 
-    Optional<TournamentStage> findTournamentStagesByCompetition(Competition competition);
+    List<TournamentStage> findTournamentStagesByCompetitionId(UUID competitionId);
 }
