ca4fc3b160b462589c78e528822d87a1bd0f92f9
Pon4ik123 <taranenkozahar@gmail.com>
2025-02-14 14:55:17 +0100

    14.02.2025 - feature/153-league-match-generation-and-tour-tracking | customized config for rabbitmq

--- backend/competition-management-service/src/main/java/org/league/app/config/RabbitMqConfig.java
+++ backend/competition-management-service/src/main/java/org/league/app/config/RabbitMqConfig.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,58 +17,126 @@ import java.util.Map;
 @Configuration
 public class RabbitMqConfig {
 
-    private final String exchange;
+    private final String tournamentExchange;
+    private final String tournamentDlxExchange;
+    private final String tournamentQueueName;
     private final String tournamentRoutingKey;
-    private final String tournamentQueue;
-    private final String tournamentDlq;
-    private final String dlx;
-    private final String tournamentRoutingKeyForDlx;
+    private final String tournamentDlxRoutingKey;
+    private final String tournamentDlqName;
 
-    public RabbitMqConfig(@Value("${rabbitmq.exchange}") String exchange,
-                          @Value("${rabbitmq.queues.tournament-start.routing-key}") String tournamentRoutingKey,
-                          @Value("${rabbitmq.queues.tournament-start.name}") String tournamentQueue,
-                          @Value("${rabbitmq.queues.tournament.dead-letter.queue.name}") String tournamentDlq,
-                          @Value("${rabbitmq.dead-letter-exchange}") String dlx,
-                          @Value("${rabbitmq.queues.tournament-start.routing-key}") String tournamentRoutingKeyForDlx) {
-        this.exchange = exchange;
+    private final String leagueExchange;
+    private final String leagueDlxExchange;
+    private final String leagueQueueName;
+    private final String leagueRoutingKey;
+    private final String leagueDlxRoutingKey;
+    private final String leagueDlqName;
+
+    public RabbitMqConfig(@Value("${rabbitmq.tournament.exchange}") String tournamentExchange,
+                          @Value("${rabbitmq.tournament.dead-letter-exchange}") String tournamentDlxExchange,
+                          @Value("${rabbitmq.tournament.queues.start.name}") String tournamentQueueName,
+                          @Value("${rabbitmq.tournament.queues.start.routing-key}") String tournamentRoutingKey,
+                          @Value("${rabbitmq.tournament.queues.start.arguments.x-dead-letter-routing-key}") String tournamentDlxRoutingKey,
+                          @Value("${rabbitmq.tournament.queues.dead-letter.queue.name}") String tournamentDlqName,
+
+                          @Value("${rabbitmq.league.exchange}") String leagueExchange,
+                          @Value("${rabbitmq.league.dead-letter-exchange}") String leagueDlxExchange,
+                          @Value("${rabbitmq.league.queues.start.name}") String leagueQueueName,
+                          @Value("${rabbitmq.league.queues.start.routing-key}") String leagueRoutingKey,
+                          @Value("${rabbitmq.league.queues.start.arguments.x-dead-letter-routing-key}") String leagueDlxRoutingKey,
+                          @Value("${rabbitmq.league.queues.dead-letter.queue.name}") String leagueDlqName) {
+
+        this.tournamentExchange = tournamentExchange;
+        this.tournamentDlxExchange = tournamentDlxExchange;
+        this.tournamentQueueName = tournamentQueueName;
         this.tournamentRoutingKey = tournamentRoutingKey;
-        this.tournamentQueue = tournamentQueue;
-        this.tournamentDlq = tournamentDlq;
-        this.dlx = dlx;
-        this.tournamentRoutingKeyForDlx = tournamentRoutingKeyForDlx;
+        this.tournamentDlxRoutingKey = tournamentDlxRoutingKey;
+        this.tournamentDlqName = tournamentDlqName;
+
+        this.leagueExchange = leagueExchange;
+        this.leagueDlxExchange = leagueDlxExchange;
+        this.leagueQueueName = leagueQueueName;
+        this.leagueRoutingKey = leagueRoutingKey;
+        this.leagueDlxRoutingKey = leagueDlxRoutingKey;
+        this.leagueDlqName = leagueDlqName;
     }
 
     @Bean
-    public DirectExchange directExchange() {
-        return new DirectExchange(exchange);
+    public DirectExchange tournamentExchange() {
+        return new DirectExchange(tournamentExchange);
+    }
+
+    @Bean
+    public DirectExchange tournamentDlx() {
+        return new DirectExchange(tournamentDlxExchange);
     }
 
     @Bean
     public Queue tournamentQueue() {
         Map<String, Object> args = new HashMap<>();
-        args.put("x-dead-letter-exchange", dlx);
-        args.put("x-dead-letter-routing-key", tournamentRoutingKeyForDlx);
-        return new Queue(tournamentQueue, true, false, false, args);
-    }
-
-    @Bean
-    public DirectExchange dlxExchange() {
-        return new DirectExchange(dlx);
+        args.put("x-dead-letter-exchange", tournamentDlxExchange);
+        args.put("x-dead-letter-routing-key", tournamentDlxRoutingKey);
+        return new Queue(tournamentQueueName, true, false, false, args);
     }
 
     @Bean
     public Queue tournamentDlq() {
-        return new Queue(tournamentDlq, true);
+        return new Queue(tournamentDlqName, true);
     }
 
     @Bean
-    public Binding dlxBinding() {
-        return BindingBuilder.bind(tournamentDlq()).to(dlxExchange()).with(tournamentRoutingKeyForDlx);
+    public Binding tournamentBinding(
+            @Qualifier("tournamentQueue") Queue tQueue,
+            @Qualifier("tournamentExchange") DirectExchange tExchange
+    ) {
+        return BindingBuilder.bind(tQueue).to(tExchange).with(tournamentRoutingKey);
     }
 
     @Bean
-    public Binding tournamentBinding() {
-        return BindingBuilder.bind(tournamentQueue()).to(directExchange()).with(tournamentRoutingKey);
+    public Binding tournamentDlqBinding(
+            @Qualifier("tournamentDlq") Queue tDlq,
+            @Qualifier("tournamentDlx") DirectExchange tDlx
+    ) {
+        return BindingBuilder.bind(tDlq).to(tDlx).with(tournamentDlxRoutingKey);
+    }
+
+
+    @Bean
+    public DirectExchange leagueExchange() {
+        return new DirectExchange(leagueExchange);
+    }
+
+    @Bean
+    public DirectExchange leagueDlx() {
+        return new DirectExchange(leagueDlxExchange);
+    }
+
+    @Bean
+    public Queue leagueQueue() {
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-dead-letter-exchange", leagueDlxExchange);
+        args.put("x-dead-letter-routing-key", leagueDlxRoutingKey);
+        return new Queue(leagueQueueName, true, false, false, args);
+    }
+
+    @Bean
+    public Queue leagueDlq() {
+        return new Queue(leagueDlqName, true);
+    }
+
+    @Bean
+    public Binding leagueBinding(
+            @Qualifier("leagueQueue") Queue lQueue,
+            @Qualifier("leagueExchange") DirectExchange lExchange
+    ) {
+        return BindingBuilder.bind(lQueue).to(lExchange).with(leagueRoutingKey);
+    }
+
+    @Bean
+    public Binding leagueDlqBinding(
+            @Qualifier("leagueDlq") Queue lDlq,
+            @Qualifier("leagueDlx") DirectExchange lDlx
+    ) {
+        return BindingBuilder.bind(lDlq).to(lDlx).with(leagueDlxRoutingKey);
     }
 
     @Bean
