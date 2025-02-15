6722ee9ba5fe8fe593be73ff19b76173d49dc430
Ivan Petrov <huko2990@gmail.com>
2025-02-16 00:33:22 +0100

    15-02-2025 - Team page connected to backend,added notification menu in main page

--- frontend/js/teamPage.js
+++ frontend/js/teamPage.js
@@ -89,3 +89,76 @@ if (document.getElementById('log-out') !== null){
   const logOutBtn = document.getElementById('log-out').addEventListener('click',logOut);
 }
 
+async function getData(){
+  try {
+    if(localStorage.getItem('MyTeam') === null && localStorage.getItem('SearchResult') === null) throw new Error('No result by search or redirecting');
+    let Team = localStorage.getItem('MyTeam')
+    if(Team === null) Team = localStorage.getItem('SearchResult');
+
+    const res = await fetch(`http://localhost:8765/team/currentTeam/${Team}`);
+    const receivedData = await res.json();
+
+    let teamInfo = receivedData.team;
+    let members = receivedData.members;
+
+    console.log(teamInfo);
+    console.log(members);
+
+    document.getElementById('team_name').innerText = `${teamInfo.teamName}`;
+    document.getElementById('createdAt').innerText = `${new Date(teamInfo.createdAt).toLocaleDateString()}`
+    let teamimg = await fetch(`http://localhost:8765/team/team-logo/${Team}`);
+    document.getElementById('team_img').src = teamimg.url;
+
+
+    members.forEach(member => {
+      // Формируем строку с ролями
+      let rolesText = member.roles.map(role => role.name).join(', ');
+
+      // Проверяем, есть ли среди ролей "MANAGER"
+      let isManager = member.roles.some(role => role.name === 'MANAGER');
+      let isCapitan = member.roles.some(role => role.name === 'CAPITAN');
+      isManager ? document.getElementById('manager_name').innerText = `${member.userId}` : '';
+      if(isManager === true){
+        document.getElementById('players').innerHTML += `
+        <div class="player">
+          <img src="img/profile.svg" alt="Avatar" class="player-avatar">
+          <div class="player-info">
+            <p class="player-name" style="color: #3861FB">Zhan Karpovich</p>
+            <p class="player-nickname">zhan_karp</p>
+            <p class="player-joined">${new Date(member.joinedAt).toLocaleDateString()}</p>
+            <p class="player-roles">${rolesText}</p>
+          </div>
+        </div>`;}
+      else if(isCapitan === true){
+        document.getElementById('players').innerHTML += `
+        <div class="player">
+          <img src="img/profile.svg" alt="Avatar" class="player-avatar">
+          <div class="player-info">
+            <p class="player-name" style="color: darkgoldenrod">Zhan Karpovich</p>
+            <p class="player-nickname">zhan_karp</p>
+            <p class="player-joined">${new Date(member.joinedAt).toLocaleDateString()}</p>
+            <p class="player-roles">${rolesText}</p>
+          </div>
+        </div>`;}
+      else{
+        document.getElementById('players').innerHTML += `
+        <div class="player">
+          <img src="img/profile.svg" alt="Avatar" class="player-avatar">
+          <div class="player-info">
+            <p class="player-name">Zhan Karpovich</p>
+            <p class="player-nickname">zhan_karp</p>
+            <p class="player-joined">${new Date(member.joinedAt).toLocaleDateString()}</p>
+            <p class="player-roles">${rolesText}</p>
+          </div>
+        </div>`;}
+      })
+
+
+
+
+
+  }catch (err){
+    console.error(err);
+  }
+}
+getData();
