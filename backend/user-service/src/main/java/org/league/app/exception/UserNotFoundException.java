20f3b126363bcf6dc821a39ee9cbe241e1d239bc
Pon4ik123 <taranenkozahar@gmail.com>
2025-02-16 21:13:51 +0100

    14.02.2025 - feature/153-league-match-generation-and-tour-tracking | added exception

---
+++ backend/user-service/src/main/java/org/league/app/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package org.league.app.exception;
+
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserNotFoundException extends UsernameNotFoundException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
