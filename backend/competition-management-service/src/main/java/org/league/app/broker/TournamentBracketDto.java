d68e33d343e143a5486995be83b6404af3b2db11
yaroslaavl <ylopatkin@gmail.com>
2025-02-18 11:55:27 +0100

    18.02.2025 - feature/156-add-tournament-match-generation | added new Dto

--- backend/competition-management-service/src/main/java/org/league/app/broker/TournamentBracketDto.java
+++ backend/competition-management-service/src/main/java/org/league/app/broker/TournamentBracketDto.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.league.app.dto.CompetitionParticipantReadDto;
 import org.league.app.dto.CompetitionReadDto;
 import org.league.app.dto.TournamentStageReadDto;
+import org.league.app.feign.sportClient.SportDto;
 
 import java.io.Serializable;
 import java.util.List;
@@ -17,5 +18,6 @@ public class TournamentBracketDto implements Serializable {
 
     public CompetitionReadDto competition;
     public List<TournamentStageReadDto> stageList;
+    public SportDto sport;
     public List<CompetitionParticipantReadDto> competitionParticipantList;
 }
