4bbe25f04c311b643a642e541b10c328e45e19fb
yaroslaavl <ylopatkin@gmail.com>
2025-02-18 11:52:35 +0100

    18.02.2025 - feature/156-add-tournament-match-generation | added custom queries

--- backend/match-service/src/main/java/org/league/app/database/repository/MatchRepository.java
+++ backend/match-service/src/main/java/org/league/app/database/repository/MatchRepository.java
@@ -1,14 +1,26 @@
 package org.league.app.database.repository;
 
 import org.league.app.database.entity.Match;
+import org.league.app.database.entity.enums.MatchStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface MatchRepository extends JpaRepository<Match, UUID> {
+
+    @Query("SELECT m FROM Match m WHERE m.matchStatus = 'SCHEDULED' AND m.competitionId IN :activeTournamentIds")
+    List<Match> findScheduledMatchesByActiveTournaments(@Param("activeTournamentIds") List<UUID> activeTournamentIds);
+
+    @Query("SELECT m FROM Match m WHERE m.matchStatus = 'IN_PROGRESS' AND m.competitionId IN :activeTournamentIds")
+    List<Match> findInProgressMatchesByActiveTournamentId(@Param("activeTournamentIds") List<UUID> activeTournamentId);
+
+    List<Match> findMatchByStageId(UUID stageId);
 
     Optional<Match> findById(UUID uuid);
 }
