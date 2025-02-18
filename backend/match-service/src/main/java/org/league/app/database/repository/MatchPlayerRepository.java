08571d89818dd7eb004c6a3bf0388e10f08cc0d6
yaroslaavl <ylopatkin@gmail.com>
2025-02-18 11:52:21 +0100

    18.02.2025 - feature/156-add-tournament-match-generation | added empty repository

---
+++ backend/match-service/src/main/java/org/league/app/database/repository/MatchPlayerRepository.java
@@ -0,0 +1,12 @@
+package org.league.app.database.repository;
+
+import org.league.app.database.entity.MatchPlayer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MatchPlayerRepository extends JpaRepository<MatchPlayer, UUID> {
+
+}
