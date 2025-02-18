8b5840a24e2361b7d4aa9eceb0cd3ad1e84ecaca
yaroslaavl <ylopatkin@gmail.com>
2025-02-18 11:55:10 +0100

    18.02.2025 - feature/156-add-tournament-match-generation | added confirm match endpoint

--- backend/match-service/src/main/java/org/league/app/controller/MatchController.java
+++ backend/match-service/src/main/java/org/league/app/controller/MatchController.java
@@ -1,17 +1,17 @@
 package org.league.app.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.league.app.dto.MatchReadDto;
 import org.league.app.service.MatchService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
-@Repository
+@Slf4j
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/match")
 public class MatchController {
@@ -22,5 +22,17 @@ public class MatchController {
     public ResponseEntity<MatchReadDto> findById(@PathVariable("matchId") UUID id) {
         MatchReadDto matchById = matchService.findMatchById(id);
         return ResponseEntity.ok(matchById);
+    }
+
+    @PutMapping("/confirmation")
+    public ResponseEntity<Boolean> confirmMatch(@RequestParam("matchId") UUID matchId,
+                                                @RequestParam(required = false, name = "teamId") UUID teamId,
+                                                @RequestParam("userId") Long userId,
+                                                @RequestParam(required = false, name = "playerIds") List<Long> playerIds) {
+        log.info("Received match confirmation request: matchId={}, teamId={}, userId={}, playerIds={}",
+                matchId, teamId, userId, playerIds);
+
+        boolean isConfirmed = matchService.matchConfirmation(matchId, teamId, userId, playerIds);
+        return ResponseEntity.ok(isConfirmed);
     }
 }
