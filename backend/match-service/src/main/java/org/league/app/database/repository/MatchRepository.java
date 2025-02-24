97c4bfdc8d8e5de597402265213be3efe2da4a1e
yaroslaavl <ylopatkin@gmail.com>
2025-02-24 21:10:42 +0100

    24.02.2025 - feature/162-show-tournament-matches-with-filters-by-competition | added JpaSpecification

--- backend/match-service/src/main/java/org/league/app/database/repository/MatchRepository.java
+++ backend/match-service/src/main/java/org/league/app/database/repository/MatchRepository.java
@@ -3,6 +3,7 @@ package org.league.app.database.repository;
 import org.league.app.database.entity.Match;
 import org.league.app.dto.ToursWithTimeGapDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface MatchRepository extends JpaRepository<Match, UUID> {
+public interface MatchRepository extends JpaRepository<Match, UUID>, JpaSpecificationExecutor<Match> {
 
     @Query("SELECT m FROM Match m WHERE m.matchStatus = 'SCHEDULED' AND m.competitionId IN :activeCompetitionIds")
     List<Match> findScheduledMatchesByActiveCompetitions(@Param("activeCompetitionIds") List<UUID> activeCompetitionIds);
