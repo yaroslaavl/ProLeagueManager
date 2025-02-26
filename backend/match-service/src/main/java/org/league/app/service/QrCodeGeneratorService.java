390e597a202f9f8ee94b95ed239c852b5273cf46
yaroslaavl <ylopatkin@gmail.com>
2025-02-26 14:20:41 +0100

    26.02.2025 - feature/174-create-QR-Code-for-match-confirmation | created service for generating qr code and write it to temp file

---
+++ backend/match-service/src/main/java/org/league/app/service/QrCodeGeneratorService.java
@@ -0,0 +1,47 @@
+package org.league.app.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.league.app.dto.QrCodeDto;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+@Slf4j
+@Service
+public class QrCodeGeneratorService {
+
+    @SneakyThrows
+    public File generateQrCode(QrCodeDto qrCodeDto) {
+        log.info("Generate QR Code for matchId={}, teamId={}, players={}, timestamp={}",
+                qrCodeDto.getMatchId(), qrCodeDto.getTeamId(), qrCodeDto.getStartingPlayers(), qrCodeDto.getTimestamp());
+
+        Map<String, Object> qrData = new HashMap<>();
+        qrData.put("matchId", qrCodeDto.getMatchId());
+        qrData.put("teamId", qrCodeDto.getTeamId());
+        qrData.put("startingPlayers", qrCodeDto.getStartingPlayers());
+        qrData.put("timeStamp", qrCodeDto.getTimestamp());
+        try {
+            String jsonString = new ObjectMapper().writeValueAsString(qrData);
+            BitMatrix bitMatrix = new QRCodeWriter().encode(jsonString, BarcodeFormat.QR_CODE, 200, 200);
+            BufferedImage bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
+
+            Path tempFile = Files.createTempFile("QRCode_", ".png");
+            ImageIO.write(bufferedImage, "png", tempFile.toFile());
+
+            return tempFile.toFile();
+        } catch (Exception e) {
+            throw new RuntimeException("Error generating QR Code", e);
+        }
+    }
+}
