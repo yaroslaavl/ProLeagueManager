93f3ba417a03a5f127722afeb0a11ec9fe69a631
yaroslaavl <ylopatkin@gmail.com>
2025-03-04 18:47:44 +0100

    04.03.2025 - feature/code-optimization | changed the level of openness of the method invoked through feign by other services

--- backend/competition-management-service/src/main/java/org/league/app/feign/authClient/AuthClientFeign.java
+++ backend/competition-management-service/src/main/java/org/league/app/feign/authClient/AuthClientFeign.java
@@ -13,29 +13,20 @@ import java.util.concurrent.TimeUnit;
 public interface AuthClientFeign {
 
     @PostMapping("/api/auth/extract-email")
-    String extractEmail(@RequestHeader(HttpHeaders.AUTHORIZATION) String token);
+    String extractEmail(@RequestHeader("Authorization") String token, @RequestParam("extractToken") String extractToken);
 
     @PostMapping("/api/auth/validate-token")
-    boolean validateToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @RequestParam("email") String email);
+    boolean validateToken(@RequestHeader("Authorization") String token, @RequestParam("validToken") String validToken, @RequestParam("email") String email);
 
     @PostMapping("/api/auth/is-access-token")
-    boolean isAccessToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String token);
+    boolean isAccessToken(@RequestHeader("Authorization") String token, @RequestParam("accessToken") String accessToken);
 
     @GetMapping("/api/auth/get-token")
     String getToken(@RequestParam("key") String key);
 
-    @PostMapping("/api/auth/set-token")
-    String setToken(@RequestParam("key") String key,
-                    @RequestParam("value") String value,
-                    @RequestParam("timeToLive") long timeToLive,
-                    @RequestParam("timeUnit") TimeUnit timeUnit);
-
-    @PostMapping("/api/auth/delete-token")
-    String deleteToken(@RequestParam("key") String key);
-
     @GetMapping("/api/auth/load-user-by-email")
-    UserDto loadUserByEmail(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @RequestParam("email") String email);
+    UserDto loadUserByEmail(@RequestHeader("Authorization") String token, @RequestParam("email") String email);
 
     @GetMapping("/api/auth/get-user-by-email")
-    UserDto getUserByEmail(@RequestParam("email") String email);
+    UserDto getUserByEmail(@RequestHeader("Authorization") String token, @RequestParam("email") String email);
 }
--- backend/sport-service/src/main/java/org/league/app/feign/AuthClientFeign.java
+++ backend/sport-service/src/main/java/org/league/app/feign/AuthClientFeign.java
@@ -7,21 +7,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.concurrent.TimeUnit;
+
 @FeignClient("user-service")
 public interface AuthClientFeign {
 
     @PostMapping("/api/auth/extract-email")
-    String extractEmail(@RequestHeader(HttpHeaders.AUTHORIZATION) String token);
+    String extractEmail(@RequestHeader("Authorization") String token, @RequestParam("extractToken") String extractToken);
 
     @PostMapping("/api/auth/validate-token")
-    boolean validateToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @RequestParam("email") String email);
+    boolean validateToken(@RequestHeader("Authorization") String token, @RequestParam("validToken") String validToken, @RequestParam("email") String email);
 
     @PostMapping("/api/auth/is-access-token")
-    boolean isAccessToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String token);
+    boolean isAccessToken(@RequestHeader("Authorization") String token, @RequestParam("accessToken") String accessToken);
 
     @GetMapping("/api/auth/get-token")
     String getToken(@RequestParam("key") String key);
 
     @GetMapping("/api/auth/load-user-by-email")
-    UserDto loadUserByEmail(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @RequestParam("email") String email);
+    UserDto loadUserByEmail(@RequestHeader("Authorization") String token, @RequestParam("email") String email);
+
+    @GetMapping("/api/auth/get-user-by-email")
+    UserDto getUserByEmail(@RequestHeader("Authorization") String token, @RequestParam("email") String email);
 }
