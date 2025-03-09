4a8bdb83a6eb85a0e53ff901436b9ff77411f253
yaroslaavl <ylopatkin@gmail.com>
2025-03-10 00:41:52 +0100

    10.03.2025 - feature/175-event-main-page | created match feign client to find upcoming top matches

---
+++ backend/calendar-event-service/src/main/java/org/league/app/feign/matchClient/MatchClientFeign.java
@@ -0,0 +1,27 @@
+package org.league.app.feign.matchClient;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+@FeignClient("match-service")
+public interface MatchClientFeign {
+
+    @Retry(name = "matchRetry", fallbackMethod = "fallbackMatch")
+    @GetMapping("/api/match/upcoming-top")
+    List<UUID> findUpcomingTopMatchesByStage(@RequestParam("isEsport") Boolean isEsport);
+
+    default List<UUID> fallbackMatch(Boolean isEsport,
+                                           Throwable t) {
+        Logger logger = LoggerFactory.getLogger(MatchClientFeign.class);
+        logger.warn("Match service returned an error. Returning empty list. isEsport={}, error={}", isEsport, t.getMessage());
+        return Collections.emptyList();
+    }
+}
