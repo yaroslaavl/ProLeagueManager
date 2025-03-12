94b1f2195df81d5a5105e3a8719822cf86052b87
yaroslaavl <ylopatkin@gmail.com>
2025-03-12 02:56:14 +0100

    12.03.2025 - feature/code-fixing | added custom exception

---
+++ backend/competition-management-service/src/main/java/org/league/app/exception/CompetitionStatusException.java
@@ -0,0 +1,7 @@
+package org.league.app.exception;
+
+public class CompetitionStatusException extends RuntimeException {
+    public CompetitionStatusException(String message) {
+        super(message);
+    }
+}
