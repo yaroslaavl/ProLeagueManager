417e66f72353ef5ef9462a1e6c47fed90e36b0a4
yaroslaavl <ylopatkin@gmail.com>
2025-03-15 23:20:19 +0100

    15.03.2025 - feature/175-event-main-page | added custom validation

---
+++ backend/calendar-event-service/src/main/java/org/league/app/validation/EventImageCustom.java
@@ -0,0 +1,21 @@
+package org.league.app.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EventImageCustomValidator.class)
+public @interface EventImageCustom {
+
+    String message() default "Image is not valid";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
