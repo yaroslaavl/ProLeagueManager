87a9222ec797344b9cafe38c44b1e062e1909a46
Pon4ik123 <taranenkozahar@gmail.com>
2025-03-18 17:19:10 +0100

    17.03.2025 - feature/189-create-containers-for-all-services-and-apis | fixed some bugs with Minio API

--- backend/calendar-event-service/src/main/java/org/league/app/service/MinioService.java
+++ backend/calendar-event-service/src/main/java/org/league/app/service/MinioService.java
@@ -27,6 +27,9 @@ public class MinioService {
     @Value("${minio.url}")
     private String minioUrl;
 
+    public static String MINIO_INTERNAL_URL = "minio";
+    public static String MINIO_PUBLIC_URL = "localhost";
+
     private final MinioClient minioClient;
     private final EventRepository eventRepository;
 
@@ -82,7 +85,9 @@ public class MinioService {
                         .build()
         );
 
-        event.setEventImage(minioUrl + "/" + bucket + "/" + objectName);
+        event.setEventImage(minioUrl.contains(MINIO_INTERNAL_URL)
+                ? minioUrl.replace(MINIO_INTERNAL_URL, MINIO_PUBLIC_URL) + "/" + bucket + "/" + objectName
+                : minioUrl + "/" + bucket + "/" + objectName);
         eventRepository.save(event);
     }
 
