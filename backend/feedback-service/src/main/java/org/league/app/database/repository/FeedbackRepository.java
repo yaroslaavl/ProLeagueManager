630023725cf557c06ee81a69d363a585d5fb0b05
yaroslaavl <ylopatkin@gmail.com>
2025-03-19 03:29:02 +0100

    19.03.2025 - feature/170-data-visualisation | added custom method to get feedbacks sorted by tonality and their count

--- backend/feedback-service/src/main/java/org/league/app/database/repository/FeedbackRepository.java
+++ backend/feedback-service/src/main/java/org/league/app/database/repository/FeedbackRepository.java
@@ -4,8 +4,10 @@ import org.league.app.database.entity.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,4 +16,7 @@ public interface FeedbackRepository extends JpaRepository<Feedback, UUID>, JpaSp
 
     @Query("SELECT f FROM Feedback f ORDER BY f.createdAt DESC")
     List<Feedback> findAllSortByCreatedAt();
+
+    @Query("SELECT f.tonality, COUNT(f) FROM Feedback f WHERE f.createdAt BETWEEN :from AND :to GROUP BY f.tonality")
+    List<Object[]> findAllByTonality(@Param("from") LocalDateTime from, @Param("to") LocalDateTime to);
 }
