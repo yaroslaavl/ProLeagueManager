85e829982fc192fdac14d3186ba41ac286575a2f
yaroslaavl <ylopatkin@gmail.com>
2025-03-19 03:27:45 +0100

    19.03.2025 - feature/170-data-visualisation | added metric configuration to tag the microservice

---
+++ backend/user-service/src/main/java/org/league/app/config/MetricsConfig.java
@@ -0,0 +1,16 @@
+package org.league.app.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config()
+                .commonTags("application", "user-service");
+    }
+}
