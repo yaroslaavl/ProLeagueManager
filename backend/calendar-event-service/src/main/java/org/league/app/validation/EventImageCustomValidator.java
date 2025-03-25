1ae276c285a7923434a4f5a52242b8282abfe28c
Pon4ik123 <taranenkozahar@gmail.com>
2025-03-25 18:27:54 +0100

    24.03.2025 - feature/193-add-validation-for-images | added validation for images in calendar-event-service

--- backend/calendar-event-service/src/main/java/org/league/app/validation/EventImageCustomValidator.java
+++ backend/calendar-event-service/src/main/java/org/league/app/validation/EventImageCustomValidator.java
@@ -6,8 +6,38 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class EventImageCustomValidator implements ConstraintValidator<EventImageCustom, MultipartFile> {
 
+    private static final long MAX_SIZE = 2 * 1024 * 1024;
+    private static final String[] CONTENT_TYPES = {"image/jpeg", "image/png", "image/jpg"};
+    public static final String[] VALID_EXTENSIONS = {".jpeg", ".jpg", ".png"};
+
     @Override
     public boolean isValid(MultipartFile file, ConstraintValidatorContext constraintValidatorContext) {
-        return true;
+        if (file == null || file.isEmpty() || file.getSize() > MAX_SIZE) {
+            return false;
+        }
+
+        String contentType = file.getContentType();
+        boolean isValidContentType = false;
+        for (String validContentType : CONTENT_TYPES) {
+            if (validContentType.equals(contentType)){
+                isValidContentType = true;
+                break;
+            }
+        }
+
+        if (!isValidContentType) {
+            return false;
+        }
+
+        String filename = file.getOriginalFilename();
+        if (filename != null) {
+            for (String extension : VALID_EXTENSIONS) {
+                if (filename.toLowerCase().endsWith(extension)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
