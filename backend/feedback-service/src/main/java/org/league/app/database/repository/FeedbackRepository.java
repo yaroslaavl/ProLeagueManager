e96996c14923a896c89fb1c99fc5ffdc70e0c991
WunderWAFL3 <huko2990@gmail.com>
2025-04-18 22:39:48 +0200

    18.04.2025 - feature/create-sport-and-esport-main-pages

--- backend/feedback-service/src/main/java/org/league/app/database/repository/FeedbackRepository.java
+++ backend/feedback-service/src/main/java/org/league/app/database/repository/FeedbackRepository.java
@@ -1,6 +1,7 @@
 package org.league.app.database.repository;
 
 import org.league.app.database.entity.Feedback;
+import org.league.app.dto.FeedbackReadDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -19,4 +20,6 @@ public interface FeedbackRepository extends JpaRepository<Feedback, UUID>, JpaSp
 
     @Query("SELECT f.tonality, COUNT(f) FROM Feedback f WHERE f.createdAt BETWEEN :from AND :to GROUP BY f.tonality")
     List<Object[]> findAllByTonality(@Param("from") LocalDateTime from, @Param("to") LocalDateTime to);
+
+    List<Feedback> findAllByCompetitionId(UUID competitionId);
 }
